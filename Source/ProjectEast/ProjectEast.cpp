42ae8ec77b3ca7676da64553952693adc18262eb
Dmitry Marchenko <>
2024-05-04 08:15:22 +0300

    feat: init project

---
+++ Source/ProjectEast/ProjectEast.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectEast.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectEast, "ProjectEast" );
