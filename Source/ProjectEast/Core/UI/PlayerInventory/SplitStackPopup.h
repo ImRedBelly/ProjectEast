0771c014f6f2f5c3ece7487280b2cf74a1e773b4
Dmitry Marchenko <>
2024-08-04 18:42:35 +0300

    feat: add SplitStackPopup

---
+++ Source/ProjectEast/Core/UI/PlayerInventory/SplitStackPopup.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SplitStackPopup.generated.h"
+
+UCLASS()
+class PROJECTEAST_API USplitStackPopup : public UUserWidget
+{
+	GENERATED_BODY()
+};
