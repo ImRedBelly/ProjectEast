b6743821e506df551c771f27ff761c9232296562
Dmitry Marchenko <>
2024-09-21 17:11:59 +0300

    feat: complete CraftingCore

--- Source/ProjectEast/Core/Components/CharacterSystems/Crafting/CraftingCore.cpp
+++ Source/ProjectEast/Core/Components/CharacterSystems/Crafting/CraftingCore.cpp
@@ -1,10 +1,9 @@
 ﻿#include "CraftingCore.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "ProjectEast/Core/Data/Inventory/MainItemData.h"
 #include "ProjectEast/Core/Utils/InventoryUtility.h"
-
-UCraftingCore::UCraftingCore()
-{
-}
 
 void UCraftingCore::BeginPlay()
 {
@@ -14,27 +13,103 @@ void UCraftingCore::BeginPlay()
 
 void UCraftingCore::ItemCrafted(AActor* OwningPlayer)
 {
+	FCraftingData* FirstItemInQueue = GetFirstItemFromQueue();
+	if (OnItemCrafted.IsBound())
+		OnItemCrafted.Broadcast(*FirstItemInQueue, OwningPlayer);
+
+	UKismetSystemLibrary::K2_ClearAndInvalidateTimerHandle(GetWorld(), CraftingTimer);
+	DecreaseCraftingCounter(FirstItemInQueue);
+
+	if (TryToStartCraftingProcess())
+		FinishCraftingProcess();
+
+	if (OnRefreshed.IsBound())
+		OnRefreshed.Broadcast();
 }
 
 void UCraftingCore::TryToCraftCurrentItem()
 {
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (IsValid(PlayerController))
+	{
+		UPlayerCrafting* PlayerCrafting = InventoryUtility::GetPlayerCrafting(PlayerController);
+
+		if (IsValid(PlayerCrafting))
+		{
+			if (PlayerCrafting->CanCraft(this))
+			{
+				PlayerCrafting->ServerTryToCraftItem(this, GetFirstItemFromQueue(), PlayerController);
+			}
+			else
+			{
+				StopCraftingProcess(GetFirstItemFromQueue(), PlayerController);
+			}
+		}
+	}
 }
 
-void UCraftingCore::SpawnCraftedItem(FCraftingData* CraftingData, AActor* OwningPlayer)
+void UCraftingCore::SpawnCraftedItem(FCraftingData* CraftingData, AActor* OwningPlayer) const
 {
+	if (OnSpawnCraftedItem.IsBound())
+		OnSpawnCraftedItem.Broadcast(*CraftingData, OwningPlayer);
 }
 
-void UCraftingCore::FailedToInitializeCraftingProcess(FCraftingData* CraftingData, uint8 AmountToCraft)
+void UCraftingCore::FailedToInitializeCraftingProcess(FCraftingData* CraftingData, int32 AmountToCraft) const
 {
+	if (OnFailedToInitialize.IsBound())
+		OnFailedToInitialize.Broadcast(*CraftingData, AmountToCraft);
 }
 
-void UCraftingCore::InitializeCraftingProcess(FCraftingData* CraftingData, uint8 AmountToCraft)
+void UCraftingCore::InitializeCraftingProcess(FCraftingData* CraftingData, int32 AmountToCraft)
 {
+	if (AmountToCraft > 0)
+	{
+		CraftingData->CraftingCounter = AmountToCraft;
+		CraftingData->MaxCount = AmountToCraft;
+		AddToCraftingQueue(CraftingData);
+	}
 }
 
-TArray<FCraftingData*> UCraftingCore::FilterByRarity(TArray<FCraftingData*> Data)
+TArray<FCraftingData*> UCraftingCore::FilterByRarity(TArray<FCraftingData*> CraftingData)
 {
-	return Data;
+	TArray<FCraftingData*> Common;
+	TArray<FCraftingData*> Superior;
+	TArray<FCraftingData*> Epic;
+	TArray<FCraftingData*> Legendary;
+
+	for (auto Data : CraftingData)
+	{
+		auto CraftableData = InventoryUtility::GetCraftableData(Data);
+		if (CraftableData.Get<0>().Num() > 0)
+		{
+			FItemData* ItemData = CraftableData.Get<0>()[0];
+			if (InventoryUtility::IsItemClassValid(ItemData))
+			{
+				switch (ItemData->Class.GetDefaultObject()->Rarity)
+				{
+				case EItemRarity::Common:
+					Common.Add(Data);
+					break;
+				case EItemRarity::Superior:
+					Superior.Add(Data);
+					break;
+				case EItemRarity::Epic:
+					Epic.Add(Data);
+					break;
+				case EItemRarity::Legendary:
+					Legendary.Add(Data);
+					break;
+				}
+			}
+		}
+	}
+	TArray<FCraftingData*> AllCraftingItems;
+	AllCraftingItems.Append(Common);
+	AllCraftingItems.Append(Superior);
+	AllCraftingItems.Append(Epic);
+	AllCraftingItems.Append(Legendary);
+
+	return AllCraftingItems;
 }
 
 void UCraftingCore::CreateCraftingList()
@@ -60,62 +135,156 @@ void UCraftingCore::CreateCraftingList()
 	CraftingList = FilterByRarity(CraftingList);
 }
 
+bool UCraftingCore::IsCrafting() const
+{
+	return bIsCrafting;
+}
+
 bool UCraftingCore::TryToStartCraftingProcess()
 {
+	FCraftingData* FirstItem = GetFirstItemFromQueue();
+	if (InventoryUtility::IsCraftingDataValid(FirstItem))
+	{
+		StartCraftingProcess(FirstItem);
+		return true;
+	}
 	return false;
 }
 
-bool UCraftingCore::CanStartCraftingProcess(FCraftingData* CraftingData)
+bool UCraftingCore::CanStartCraftingProcess(FCraftingData* CraftingData) const
 {
-	return false;
+	return !UKismetSystemLibrary::K2_TimerExistsHandle(GetWorld(), CraftingTimer) &&
+		CraftingData->CraftingCounter > 0;
 }
 
 bool UCraftingCore::StartCraftingProcess(FCraftingData* CraftingData)
 {
+	if (CanStartCraftingProcess(CraftingData))
+	{
+		StartCraftingTimer(CraftingData->CraftingTime);
+		bIsCrafting = true;
+		if (OnCraftingProcessStarted.IsBound())
+			OnCraftingProcessStarted.Broadcast(*CraftingData);
+
+		return true;
+	}
 	return false;
 }
 
 void UCraftingCore::StopCraftingProcess(FCraftingData* CraftingData, AActor* OwningPlayer)
 {
+	if (IsCurrentlyCrafted(CraftingData))
+	{
+		UKismetSystemLibrary::K2_ClearAndInvalidateTimerHandle(GetWorld(), CraftingTimer);
+		bIsCrafting = false;
+	}
+	UPlayerCrafting* PlayerCrafting = InventoryUtility::GetPlayerCrafting(OwningPlayer);
+	if (IsValid(PlayerCrafting))
+		PlayerCrafting->ServerReturnSecuredMaterials(CraftingData, this, OwningPlayer);
+
+	if (OnCraftingProcessStopped.IsBound())
+		OnCraftingProcessStopped.Broadcast(*CraftingData);
+	RemoveFromCraftingQueue(CraftingData);
 }
 
 void UCraftingCore::FinishCraftingProcess()
 {
+	bIsCrafting = false;
+	FCraftingData* FirstItem = GetFirstItemFromQueue();
+	if (OnCraftingProcessFinished.IsBound())
+		OnCraftingProcessFinished.Broadcast(*FirstItem);
+	RemoveFromCraftingQueue(FirstItem);
 }
 
-TTuple<float, float> UCraftingCore::GetCraftingProcessTime()
+TTuple<float, float> UCraftingCore::GetCraftingProcessTime() const
 {
+	if (UKismetSystemLibrary::K2_IsTimerActiveHandle(GetWorld(), CraftingTimer))
+	{
+		auto CurrentTime = UKismetSystemLibrary::K2_GetTimerElapsedTimeHandle(GetWorld(), CraftingTimer);
+		auto LeftTime = UKismetSystemLibrary::K2_GetTimerRemainingTimeHandle(GetWorld(), CraftingTimer);
+		return MakeTuple(CurrentTime, LeftTime);
+	}
+
 	return MakeTuple(0, 0);
 }
 
-void UCraftingCore::AddToCraftingQueue(TArray<FCraftingData*> AddData)
+void UCraftingCore::AddToCraftingQueue(FCraftingData* AddData)
 {
+	if (!IsCraftableItemInQueue(AddData))
+	{
+		auto Quantity = CraftingQueue.AddUnique(AddData);
+		if (OnAddedToQueue.IsBound())
+			OnAddedToQueue.Broadcast(*AddData, Quantity);
+	}
+	else
+	{
+		auto ModifyData = ModifyCraftingCounter(AddData->CraftingID, AddData->CraftingCounter, AddData->MaxCount);
+		if (OnAddedToQueue.IsBound())
+			OnAddedToQueue.Broadcast(*ModifyData, GetItemQueueIndex(AddData->CraftingID));
+	}
+	TryToStartCraftingProcess();
 }
 
 void UCraftingCore::RemoveFromCraftingQueue(FCraftingData* RemoveData)
 {
+	int32 Index = GetItemQueueIndex(RemoveData->CraftingID);
+	if (Index > -1)
+	{
+		CraftingQueue.RemoveAt(Index);
+		if (OnRemoveFromQueue.IsBound())
+			OnRemoveFromQueue.Broadcast(*RemoveData, Index);
+		TryToStartCraftingProcess();
+	}
 }
 
 void UCraftingCore::ClearCraftingQueue(AActor* OwningPlayer)
 {
+	UPlayerCrafting* PlayerCrafting = InventoryUtility::GetPlayerCrafting(OwningPlayer);
+	if (IsValid(PlayerCrafting))
+	{
+		UKismetSystemLibrary::K2_ClearAndInvalidateTimerHandle(GetWorld(), CraftingTimer);
+		bIsCrafting = false;
+
+		for (auto CraftingData : CraftingQueue)
+			PlayerCrafting->ServerReturnSecuredMaterials(CraftingData, this, OwningPlayer);
+		CraftingQueue.Empty();
+		if (OnClearedQueue.IsBound())
+			OnClearedQueue.Broadcast();
+		if (OnRefreshed.IsBound())
+			OnRefreshed.Broadcast();
+	}
 }
 
-FCraftingData* UCraftingCore::GetItemFromQueueByID()
+TTuple<bool, FCraftingData*> UCraftingCore::GetItemFromQueueByID(FString ID)
 {
-	FCraftingData* CraftingData = new FCraftingData();
-	return CraftingData;
+	int32 Index = GetItemQueueIndex(ID);
+	if (Index > -1)
+	{
+		return MakeTuple(true, CraftingQueue[Index]);
+	}
+
+	return MakeTuple(false, &EmptyCraftingData);
 }
 
 FCraftingData* UCraftingCore::GetFirstItemFromQueue()
 {
-	FCraftingData* CraftingData = new FCraftingData();
-	return CraftingData;
+	if (CraftingQueue.IsValidIndex(0))
+		return CraftingQueue[0];
+	return &EmptyCraftingData;
 }
 
-FCraftingData* UCraftingCore::ModifyCraftingCounter(FString CraftingID, uint8 CraftingCounter, uint8 MaxCount)
+FCraftingData* UCraftingCore::ModifyCraftingCounter(FString CraftingID, int32 CraftingCounter, int32 MaxCount)
 {
-	FCraftingData* CraftingData = new FCraftingData();
-	return CraftingData;
+	int32 Index = GetItemQueueIndex(CraftingID);
+	if (Index > -1)
+	{
+		FCraftingData* Data = InventoryUtility::CopyCraftingData(CraftingQueue[Index]);
+		Data->CraftingCounter += CraftingCounter;
+		Data->MaxCount += MaxCount;
+		return Data;
+	}
+
+	return &EmptyCraftingData;
 }
 
 TArray<FCraftingData*> UCraftingCore::GetCraftingListArray()
@@ -123,41 +292,84 @@ TArray<FCraftingData*> UCraftingCore::GetCraftingListArray()
 	return CraftingList;
 }
 
-void UCraftingCore::StartCraftingTimer(float StartTimer)
+void UCraftingCore::StartCraftingTimer(float CraftingTime)
 {
+	float TimeValue = CraftingTime * (1 / CraftingDurationRate);
+	float Time = FMathf::Clamp(TimeValue, 1, TimeValue);
+	GetWorld()->GetTimerManager().SetTimer(CraftingTimer, this, &UCraftingCore::TryToCraftCurrentItem, Time, true, 0);
 }
 
 bool UCraftingCore::IsCurrentlyCrafted(FCraftingData* CraftingData)
 {
-	return false;
+	return GetFirstItemFromQueue()->CraftingID == CraftingData->CraftingID;
 }
 
 void UCraftingCore::DecreaseCraftingCounter(FCraftingData* CraftingData)
 {
+	if (InventoryUtility::IsCraftingDataValid(CraftingData))
+	{
+		auto Index = GetItemQueueIndex(CraftingData->CraftingID);
+		if (Index > -1)
+		{
+			CraftingQueue[Index]->CraftingCounter -= 1;
+		}
+	}
 }
 
-bool UCraftingCore::HasPlayerEnoughGold(FCraftingData* CraftingData, uint8 AmountToCraft, AActor* OwningPlayer)
+bool UCraftingCore::HasPlayerEnoughGold(FCraftingData* CraftingData, int32 AmountToCraft, AActor* OwningPlayer) const
 {
-	return false;
+	UPlayerInventory* PlayerInventory = InventoryUtility::GetPlayerInventory(OwningPlayer);
+	if (IsValid(PlayerInventory))
+	{
+		return PlayerInventory->GetOwnerGold() > AmountToCraft * (CraftingData->CraftingCost * CraftingCostMultiplier);
+	}
+	return true;
 }
 
 bool UCraftingCore::IsCraftableItemInQueue(FCraftingData* CraftingData)
 {
+	if (InventoryUtility::IsCraftingDataValid(CraftingData))
+	{
+		return GetItemFromQueueByID(CraftingData->CraftingID).Get<0>();
+	}
 	return false;
 }
 
-uint8 UCraftingCore::GetItemQueueIndex(FString CraftingID)
+int32 UCraftingCore::GetItemQueueIndex(FString CraftingID)
 {
-	return 0;
+	for (int i = 0; i < CraftingQueue.Num(); ++i)
+	{
+		if (CraftingQueue[i]->CraftingID == CraftingID)
+		{
+			return i;
+		}
+	}
+	return -1;
 }
 
-bool UCraftingCore::CanBeAddedToQueue()
+bool UCraftingCore::CanBeAddedToQueue() const
 {
-	return false;
+	return CraftingQueue.Num() < MaxQueuedItems;
 }
 
-TTuple<bool, FText> UCraftingCore::CanInitializeCraftingProcess(FCraftingData* CraftingData, uint8 AmountToCraft,
+TTuple<bool, FText> UCraftingCore::CanInitializeCraftingProcess(FCraftingData* CraftingData, int32 AmountToCraft,
                                                                 AActor* OwningPlayer)
 {
-	return MakeTuple(false, FText());
+	if (bCanCraftItems)
+	{
+		if (CanBeAddedToQueue())
+		{
+			if (!IsCurrentlyCrafted(CraftingData))
+			{
+				if (HasPlayerEnoughGold(CraftingData, AmountToCraft, OwningPlayer))
+				{
+					return MakeTuple(true, FText::FromString(""));
+				}
+				return MakeTuple(false, FText::FromString("Not enough Gold."));
+			}
+			return MakeTuple(false, FText::FromString("Item is currently being Crafted"));
+		}
+		return MakeTuple(false, FText::FromString("Queue is full."));
+	}
+	return MakeTuple(false, FText::FromString("Item cannot be Crafted."));
 }
--- Source/ProjectEast/Core/Components/CharacterSystems/Crafting/CraftingCore.h
+++ Source/ProjectEast/Core/Components/CharacterSystems/Crafting/CraftingCore.h
@@ -12,54 +12,72 @@ class PROJECTEAST_API UCraftingCore : public UActorComponent
 	GENERATED_BODY()
 
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnRefreshed);
-	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnClearedQueue);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCraftingProcessStarted, FCraftingData&, CraftingData);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCraftingProcessStopped, FCraftingData&, CraftingData);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCraftingProcessFinished, FCraftingData&, CraftingData);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnItemCrafted, FCraftingData&, CraftingData, AActor*, OwnningPlayer);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAddedToQueue, FCraftingData&, CraftingData, int32, Quantity);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnRemoveFromQueue, FCraftingData&, CraftingData, int32, Quantity);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnFailedToInitialize, FCraftingData&, CraftingData, int32, Quantity);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnSpawnCraftedItem, FCraftingData&, CraftingData, AActor*, OwnningPlayer);
+
 public:
-	UCraftingCore();
+	FOnRefreshed OnRefreshed;
+	FOnClearedQueue OnClearedQueue;
+	FOnCraftingProcessStarted OnCraftingProcessStarted;
+	FOnCraftingProcessStopped OnCraftingProcessStopped;
+	FOnCraftingProcessFinished OnCraftingProcessFinished;
+	FOnItemCrafted OnItemCrafted;
+	FOnAddedToQueue OnAddedToQueue;
+	FOnRemoveFromQueue OnRemoveFromQueue;
+	FOnFailedToInitialize OnFailedToInitialize;
+	FOnSpawnCraftedItem OnSpawnCraftedItem;
 
 	virtual void BeginPlay() override;
+
 	void ItemCrafted(AActor* OwningPlayer);
 	void TryToCraftCurrentItem();
-	void SpawnCraftedItem(FCraftingData* CraftingData, AActor* OwningPlayer);
-	void FailedToInitializeCraftingProcess(FCraftingData* CraftingData, uint8 AmountToCraft);
-
-	void InitializeCraftingProcess(FCraftingData* CraftingData, uint8 AmountToCraft);
-	TArray<FCraftingData*>  FilterByRarity(TArray<FCraftingData*> Data);
+	void SpawnCraftedItem(FCraftingData* CraftingData, AActor* OwningPlayer) const;
+	void FailedToInitializeCraftingProcess(FCraftingData* CraftingData, int32 AmountToCraft) const;
+	void InitializeCraftingProcess(FCraftingData* CraftingData, int32 AmountToCraft);
 	void CreateCraftingList();
-
-	bool TryToStartCraftingProcess();
-	bool CanStartCraftingProcess(FCraftingData* CraftingData);
-	bool StartCraftingProcess(FCraftingData* CraftingData);
 	void StopCraftingProcess(FCraftingData* CraftingData, AActor* OwningPlayer);
 	void FinishCraftingProcess();
-
-	TTuple<float, float> GetCraftingProcessTime();
-	int32 GetCraftingQueueLength() const {return CraftingQueue.Num();}
-	void AddToCraftingQueue(TArray<FCraftingData*> AddData);
+	void AddToCraftingQueue(FCraftingData* AddData);
 	void RemoveFromCraftingQueue(FCraftingData* RemoveData);
 	void ClearCraftingQueue(AActor* OwningPlayer);
-
-	FCraftingData* GetItemFromQueueByID();
-	FCraftingData* GetFirstItemFromQueue();
-	FCraftingData* ModifyCraftingCounter(FString CraftingID, uint8 CraftingCounter, uint8 MaxCount);
-	TArray<FCraftingData*> GetCraftingListArray();
-	void StartCraftingTimer(float StartTimer);
-
-	bool IsCurrentlyCrafted(FCraftingData* CraftingData);
+	void StartCraftingTimer(float CraftingTime);
 	void DecreaseCraftingCounter(FCraftingData* CraftingData);
-	bool HasPlayerEnoughGold(FCraftingData* CraftingData, uint8 AmountToCraft, AActor* OwningPlayer);
-	bool IsCraftableItemInQueue(FCraftingData* CraftingData);
-	uint8 GetItemQueueIndex(FString CraftingID);
-	bool CanBeAddedToQueue();
-	int32 GetMaxQueuedItems() const {return MaxQueuedItems;}
-	bool GetCanCraftItems() const {return CanCraftItems;}
 
-	TTuple<bool, FText> CanInitializeCraftingProcess(FCraftingData* CraftingData, uint8 AmountToCraft,
+	FCraftingData* GetFirstItemFromQueue();
+	FCraftingData* ModifyCraftingCounter(FString CraftingID, int32 CraftingCounter, int32 MaxCount);
+
+	TArray<FCraftingData*> GetCraftingListArray();
+	static TArray<FCraftingData*> FilterByRarity(TArray<FCraftingData*> CraftingData);
+
+	TTuple<bool, FCraftingData*> GetItemFromQueueByID(FString ID);
+	TTuple<float, float> GetCraftingProcessTime() const;
+	TTuple<bool, FText> CanInitializeCraftingProcess(FCraftingData* CraftingData, int32 AmountToCraft,
 	                                                 AActor* OwningPlayer);
-	bool GetIsShowLockedItems() const { return bIsShowLockedItems; }
 
-	
-	FOnRefreshed OnRefreshed;
-	
+	bool IsCrafting() const;
+	bool CanBeAddedToQueue() const;
+	bool TryToStartCraftingProcess();
+	bool IsCurrentlyCrafted(FCraftingData* CraftingData);
+	bool StartCraftingProcess(FCraftingData* CraftingData);
+	bool IsCraftableItemInQueue(FCraftingData* CraftingData);
+	bool CanStartCraftingProcess(FCraftingData* CraftingData) const;
+	bool HasPlayerEnoughGold(FCraftingData* CraftingData, int32 AmountToCraft, AActor* OwningPlayer) const;
+
+	int32 GetItemQueueIndex(FString CraftingID);
+	int32 GetCraftingQueueLength() const { return CraftingQueue.Num(); }
+
+	bool GetIsShowLockedItems() const { return bIsShowLockedItems; }
+	bool GetCanCraftItems() const { return bCanCraftItems; }
+	int32 GetMaxQueuedItems() const { return MaxQueuedItems; }
+	float GetCraftingCostMultiplier() const { return CraftingCostMultiplier; }
+
 protected:
 	UPROPERTY(EditAnywhere)
 	TArray<UDataTable*> CraftingDataTables;
@@ -67,25 +85,33 @@ protected:
 	TArray<FDataTableRowHandle> SingleDTItems;
 
 	UPROPERTY(EditAnywhere)
+	ECraftingStation CraftingStation;
+
+	UPROPERTY(EditAnywhere)
+	bool bSpawnCraftedItem;
+	UPROPERTY(EditAnywhere)
 	bool bIsShowLockedItems;
 	UPROPERTY(EditAnywhere)
-	int32 MaxQueuedItems;
+	bool CraftOnlyWhenWindowIsOpen;
 	UPROPERTY(EditAnywhere)
-	bool CanCraftItems;
+	bool bCanCraftItems;
+
+	UPROPERTY(EditAnywhere)
+	float CraftingCostMultiplier = 1;
+
+	UPROPERTY(EditAnywhere)
+	float CraftingDurationRate;
+
+	UPROPERTY(EditAnywhere)
+	int32 MaxQueuedItems;
+
+	UPROPERTY(EditAnywhere)
+	FCraftingData& EmptyCraftingData;
 
 private:
 	TArray<FCraftingData*> CraftingList;
 	TArray<FCraftingData*> CraftingQueue;
-	
-	ECraftingStation CraftingStation;
 
 	FTimerHandle CraftingTimer;
-
-	float CraftingCostMultiplier;
-	float CraftingDurationRate;
-
-	bool CraftOnlyWhenWindowIsOpen;
-	bool bCanCraftItems;
-	bool bSpawnCraftedItem;
 	bool bIsCrafting;
 };
